081f19b750e7e2dc03f8a279598e7ad70337bd07
Dima Devgru <git@devg.ru>
2018-02-11 17:27:49 +0300

    Separate rectircle into util

--- src/presentational/Slots/index.js
+++ src/presentational/Slots/index.js
@@ -2,49 +2,48 @@ import React from 'react';
 import {max} from 'd3-array';
 
 import farthestOf from '../../utils/farthestOf';
+import rectircle from '../../utils/rectircle';
 
-const SIZE = 100;
-const CENTER = SIZE / 2;
-const STEP = SIZE / 10;
+const SIZE = 72;
 
 const X_STEP = SIZE / 10;
 const Y_STEP = SIZE / 25;
 const WIDTH = SIZE + X_STEP * 2;
 const HEIGHT = SIZE + Y_STEP * 2;
-const SQUIRCLE_D = `M ${CENTER},0 C ${STEP},0 0,${STEP} 0,${CENTER} 0,${SIZE - STEP} ${STEP},${SIZE} ${CENTER},${SIZE} ${SIZE - STEP},${SIZE} ${SIZE},${SIZE - STEP} ${SIZE},${CENTER} ${SIZE},${STEP} ${SIZE - STEP},0 ${CENTER},0 Z`;
 
-const Slots = ({ colors, slots, textColors }) => (
-  <svg
-    className="SlotsLine"
-    style={{
-      clear: 'both',
-    }}
-    width={WIDTH * slots.length}
-    height={HEIGHT * max(slots, ({indices}) => indices.length)}
-  >
-    <defs>
-      <path
-        id="squircle"
-        d={SQUIRCLE_D}
-      />
-    </defs>
-    {slots.map((slot, i) =>
-      <g
-        key={i}
-        transform={`translate(${i * WIDTH})`}
-      >
-        {slot.indices.map((colorIndex, j) =>
-          <use
-            key={colorIndex}
-            transform={`translate(${X_STEP}, ${Y_STEP + j * HEIGHT})`}
-            xlinkHref="#squircle"
-            fill={colors[colorIndex]}
-            stroke={farthestOf(colors[colorIndex], textColors)}
-          />
-        )}
-      </g>
-    )}
-  </svg>
-);
+const Slots = ({ colors, slots, textColors }) => {
+  const maxSlots = max(slots, ({indices}) => indices.length);
+  return (
+    <svg
+      className="SlotsLine"
+      style={{
+        clear: 'both',
+      }}
+      width={WIDTH * slots.length}
+      height={HEIGHT * maxSlots}
+    >
+      {slots.map((slot, i) =>
+        <g
+          key={i}
+          transform={`translate(${i * WIDTH})`}
+        >
+          {slot.indices.map((colorIndex, j) => {
+            const slotsCount = slot.indices.length;
+            const height = SIZE * maxSlots / slotsCount + (maxSlots - slotsCount) * Y_STEP * 2;
+            return (
+              <path
+                key={colorIndex}
+                transform={`translate(${X_STEP}, ${Y_STEP + j * HEIGHT})`}
+                d={rectircle(SIZE, height)}
+                fill={colors[colorIndex]}
+                stroke={farthestOf(colors[colorIndex], textColors)}
+              />
+            );
+          })}
+        </g>
+      )}
+    </svg>
+  )
+};
 
 export default Slots;
---
+++ src/utils/rectircle.js
@@ -0,0 +1,33 @@
+// noinspection JSSuspiciousNameCombination
+// Like squircle, but rectangular.
+const Rectircle = (width, height = width) => {
+  const size = Math.min(width, height);
+  console.log(width, height);
+
+  const c = size / 60;
+
+  const hCenter = width / 2;
+  const vCenter = height / 2;
+  const hDiff = (width - size) / 2;
+  const vDiff = (height - size) / 2;
+
+  //    a  h
+  // b        g
+  //
+  // c        f
+  //    d  e
+
+  return [
+    `M ${hCenter - hDiff},0`, // a
+    `C ${c},0 0,${c} 0,${vCenter - vDiff}`,  // b
+    `L 0,${vCenter + vDiff}`, // c
+    `C 0,${height - c} ${c},${height} ${hCenter - hDiff},${height}`, // d
+    `L ${hCenter + hDiff},${height}`, // e
+    `C ${width - c},${height} ${width},${height - c} ${width},${vCenter + vDiff}`, // f
+    `L ${width},${vCenter - vDiff}`, // g
+    `C ${width},${c} ${width - c},0 ${hCenter + hDiff},0`, // h
+    'Z',
+  ].join(' ');
+};
+
+export default Rectircle;
