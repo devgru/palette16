470c17ef3bd38083607fa9646b56a9bf90eace6d
Dima Devgru <git@devg.ru>
2018-04-18 00:59:15 +0300

    Rename fn

--- src/utils/toHex.js
+++ src/utils/toHex.js
@@ -3,6 +3,6 @@ import rgb2hex from 'closest-color/src/rgb2hex';
 
 export default function toHex(color) {
   const { r, g, b } = rgb(color);
-  const z = v => Math.max(0, Math.min(255, Math.round(v)));
-  return rgb2hex({ R: z(r), G: z(g), B: z(b) });
+  const fit = v => Math.max(0, Math.min(255, Math.round(v)));
+  return rgb2hex({ R: fit(r), G: fit(g), B: fit(b) });
 }
